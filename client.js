86299616d7c5960c983b33410cc793b4239f66a0
unknown <>
2016-12-23 18:58:14 +0900

    장훈이 버스 부릉부릉

--- client.js
+++ client.js
@@ -17,16 +17,36 @@ $(".btn").click(function(){
         name : nickname
     })
     $("#input").val('')
+});
+setInterval(()=>{
+    socket.emit('People',{
+        name : nickname
+    });
+},5000)
+$(".Wbtn").click(()=>{
+    let nick = prompt("귓속말을 보낼 상대","");
+    let msg = prompt("메세지","");
+    socket.emit('Whisper',{
+        message : msg,
+        Your : nick,
+        My : nickname
+    });
+    $("#list").append(`(${nick}에게) : ${msg}`+'<br>');
 })
 $("#input").keydown((ev)=>{
     if(ev.keyCode == 13){
         socket.emit('text',{
-        message : $("#input").val(),
-        name : nickname
-    })
-    $("#input").val('')
+            message : $("#input").val(),
+            name : nickname
+        })
+        $("#input").val('').focus();
     }
-})
+});
 socket.on('returnMessage', (data) =>{
     $("#list").append(data.name +" : "+data.message + '<br>');
 });
+socket.on('returnWhisper', (data)=>{
+    if(data.who == nickname){
+        $("#list").append("(귓속말)"+data.data.My +" : "+data.data.message + '<br>');
+    }
+})
--- index.js
+++ index.js
@@ -1,13 +1,41 @@
 let io = require('socket.io')(9999);
+let Plist = {};
+let account = 0;
 
 io.on('connection', (client)=>{
-    
     console.log(`클라이언트 접속 : ${client.id}`);
+    account++;
     client.emit('welcome message', {message : "Welcome"});
     client.on('disconnect', ()=>{
          console.log(`클라이언트 접속 해제 : ${client.id}`);
+         Plist[client.id] = "";
+         account--;
     });
     client.on('text',(data)=>{
         io.emit('returnMessage', data);
+        console.log(`${client.id}(${data.name}) : ${data.message}`);
+    });
+    client.on('People', (data)=>{
+        Plist[client.id] = {
+            nickname : data.name
+        };
+        console.log(`접속자 수 : ${account}`);
+        console.log("접속자 : ")
+        for(a in Plist){
+            if(Plist[a] != "")
+                console.log(Plist[a].nickname)
+        }
     })
+    client.on('Whisper', (data)=>{
+        console.log(`귓속말 : ${Plist[client.id].nickname} to ${data.Your} : ${data.message}`)
+            for(var a in Plist){
+            if(Plist[a] != "")
+                if(Plist[a].nickname == data.Your){
+                    io.emit('returnWhisper', {
+                        data,
+                        who : data.Your
+                    })
+                }
+            }
+    });
 });
